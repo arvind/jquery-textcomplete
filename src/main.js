62893160c2f3498d4bcd19b4fba2b4a5107b13b8
Arvind Satyanarayan <arvindsatya1@gmail.com>
2016-04-11 17:33:10 -0700

    Convert main.js and strategy.js

--- src/main.js
+++ src/main.js
@@ -5,57 +5,40 @@
  * License:    MIT (https://github.com/yuku-t/jquery-textcomplete/blob/master/LICENSE)
  * Author:     Yuku Takahashi
  */
+'use strict';
 
-if (typeof jQuery === 'undefined') {
-  throw new Error('jQuery.textcomplete requires jQuery');
-}
+var Completer = require('./Completer'),
+    Strategy  = require('./Strategy'),
+    util = require('./util'),
+    counter = 0, registry = {};
 
-+function ($) {
-  'use strict';
+/**
+ * Initialize the TextComplete interaction.
+ * @param  {DomElement[]} elems An array of DOM element nodes
+ * (e.g., selected with document.querySelectorAll).
+ * @param  {Strategy} strategies TextComplete Strategy definition
+ * @param  {Object} options Customize TextComplete interaction.
+ * @return {void}
+ */
+module.exports = function TextComplete(elems, strategies, options) {
+  elems.forEach(function(el) {
+    var data = el.dataset,
+        completer = registry[data.textComplete];
 
-  var warn = function (message) {
-    if (console.warn) { console.warn(message); }
-  };
+    if (!completer) {
+      options = options || {};
+      options._oid = ++counter;  // unique object id
+      completer = registry[counter] = new Completer(el, options);
+    }
 
-  var id = 1;
-
-  $.fn.textcomplete = function (strategies, option) {
-    var args = Array.prototype.slice.call(arguments);
-    return this.each(function () {
-      var self = this;
-      var $this = $(this);
-      var completer = $this.data('textComplete');
-      if (!completer) {
-        option || (option = {});
-        option._oid = id++;  // unique object id
-        completer = new $.fn.textcomplete.Completer(this, option);
-        $this.data('textComplete', completer);
+    if (typeof strategies === 'string') {
+      if (!completer) return;
+      completer[strategies].call(completer, options);
+      if (strategies === 'destroy') {
+        delete data.textComplete;
       }
-      if (typeof strategies === 'string') {
-        if (!completer) return;
-        args.shift()
-        completer[strategies].apply(completer, args);
-        if (strategies === 'destroy') {
-          $this.removeData('textComplete');
-        }
-      } else {
-        // For backward compatibility.
-        // TODO: Remove at v0.4
-        $.each(strategies, function (obj) {
-          $.each(['header', 'footer', 'placement', 'maxCount'], function (name) {
-            if (obj[name]) {
-              completer.option[name] = obj[name];
-              warn(name + 'as a strategy param is deprecated. Use option.');
-              delete obj[name];
-            }
-          });
-        });
-        completer.register($.fn.textcomplete.Strategy.parse(strategies, {
-          el: self,
-          $el: $this
-        }));
-      }
-    });
-  };
-
-}(jQuery);
+    } else {
+      completer.register(Strategy.parse(strategies, el));
+    }
+  });
+};
